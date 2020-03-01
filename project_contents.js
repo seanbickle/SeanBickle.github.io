2855cec129f89d509f5e56c827d44f11e11051a5
SeanBickle <sean.a.bickle@gmail.com>
2020-03-01 02:46:46 +0000

    Spelling mistakes and favicon

--- project_contents.js
+++ project_contents.js
@@ -5,21 +5,21 @@ var projects = [
     {
         title: "Movie Poster Text",
         badges: [python_badge, "OpenCV"],
-        body: "A simple python application to generate a movie poster from a passage of text.",
+        body: "A python application to generate a movie poster from a passage of text. I hope to return to this project to build some kind of preview interface.",
         link: "https://github.com/SeanBickle/moviepostertext",
         link_text: "<i class='fab fa-github'></i> Repo",
     },
     {
         title: "Melons",
         badges: [js_badge],
-        body: "A single-player melon-farming game created in 24 hours during a university game-jam. This won 'Best use of theme', which was 'Time'.",
+        body: "A single-player melon-farming game created in 24 hours during a university game-jam. This won 'Best use of Theme', which was 'Time'.",
         link: "melons/index.htm",
         link_text: "<i class='fas fa-gamepad'></i> Play"
     },
     {
         title: "Pi Estimator",
         badges: [js_badge],
-        body: "A simple visual estimator of Pi.",
+        body: "A simple visual estimator of Pi. I think the record for most iterations that someone sent me is about 3.5 million.",
         link: "pi/index.htm",
     },
     {
